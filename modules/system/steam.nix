6ba093f53f6b79c7b451c16e4e6cdf9fdbb01668
lanikea <sum1hewe@gmail.com>
2026-01-11 20:09:52 +0800

    fixed xwayland-sattelite for niri

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -3,7 +3,7 @@
   config = lib.mkIf config.sysconfig.gaming.enable {
     programs.steam.enable = true;
 
-    environment.systemPackages = [pkgs.protonup-ng pkgs.xwayland-satellite];
+    environment.systemPackages = [pkgs.protonup-ng];
     environment.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = 
         "/home/${variables.username}/.steam/.root/compatibilitytools.d";
